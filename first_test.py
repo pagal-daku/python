66cdb256649e5912d2530e95366dc40915a3ff05
Pagal Daku <pagaldaku@outlook.com>
2021-03-25 17:48:13 +0530

    add new code string operation

--- first_test.py
+++ first_test.py
@@ -2,3 +2,24 @@ print("hello world!!")
 
 print("In line printing")
 print("this is second branch - calc divide")
+
+message = "hello worlSd"
+print(message[0:5])
+print(message.upper())
+print(message.lower())
+
+greeting = 'hello'
+name = 'daku'
+
+#regular printing data
+message = greeting + ',' + name + ' welcome!!'
+
+#{}
+message = '{},{} welcome!'. format(greeting,name)
+
+#with f strting 3.6 +
+messageWithFstring = f'{greeting} , {name.upper()} .welcome !'
+print(message)
+print(messageWithFstring)
+
+
