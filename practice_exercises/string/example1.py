6657b266f0ec4a4bd5b056e9a67ff53fb1168a68
Pagal Daku <pagaldaku@outlook.com>
2021-03-27 20:31:42 +0530

    Practice example folders and subfolder

---
+++ practice_exercises/string/example1.py
@@ -0,0 +1,26 @@
+#Program 1 - With Python inbuild method
+
+#Write a python program find the number of characters present in a string with help of len()
+
+findLengthWithLen = "This is find number of characters present in a string using len function"
+
+print(len(findLengthWithLen))
+#output = 72
+
+
+
+
+#Program 1 -  Without  Python inbuild method
+
+#Write a python program find the number of characters present in a string (with out using len())
+
+#string
+findLengthWithOutLen = "This is find number of characters present in a string without using len function"
+
+#count = 0
+#use for loop to travse the string
+
+count = 0
+for string in findLengthWithOutLen:
+    count = count+1
+print(count)
