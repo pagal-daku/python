5af7335b771610a0e68d8e8a5524b63876f53b1e
Pagal Daku <pagaldaku@outlook.com>
2021-03-27 17:36:54 +0530

    new add

--- string.py
+++ string.py
@@ -28,3 +28,22 @@ if 'x' in a:
     print("true")
 else:
     print("false")
+
+# String count
+
+print(a.count('p'))
+print(a.count('y'))
+print(a.count('a'))
+print(a.count('t'))
+
+#string count first veriable , start position , end position
+print(a.count('t',6,22))
+
+#find position
+
+print(a.find('n'))
+
+print(a.rfind('q'))
+print(a.rfind('n'))
+
+print(a.find('training'))
