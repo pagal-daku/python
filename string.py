97a91b8aaa801a2686644b780a7e873a88c4fc8c
Pagal Daku <pagaldaku@outlook.com>
2021-03-27 15:22:43 +0530

    String practice

---
+++ string.py
@@ -0,0 +1,30 @@
+a = "python string practice"
+
+#length of particular string
+print(len(a))
+
+#
+print(max(a))
+print(min(a))
+
+#position in string
+print(a[0])
+
+#from to to position string
+print(a[0:2])
+
+#concat string
+string1 = 'python'
+string2 = ' tutorial'
+
+print(string1 + string2)
+
+#print same string multiple times
+t = "demo "
+print(t  * 2)
+
+#check value present in the string
+if 'x' in a:
+    print("true")
+else:
+    print("false")
