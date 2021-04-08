d452d674e4ab8b0e214efc78bba3f92fa5a8893f
Pagal Daku <pagaldaku@outlook.com>
2021-04-08 22:16:19 +0530

    set example

---
+++ topic/data_type/set.py
@@ -0,0 +1,8 @@
+#Sets are unordered.
+#Set elements are unique. Duplicate elements are not allowed.
+#A set itself may be modified, but the elements contained in the set must be of an immutable type.
+
+s = {"abc", 34, True, 40, "male"}
+
+print(type(p))
+print(type(s))
